22f92068cbe063e6ed605e2b42e4775e6de4b357
Hua Sun <33794673+hsun9@users.noreply.github.com>
2025-06-17 11:04:26 -0400

    Add files via upload

---
+++ calinfercnv/run.py
@@ -0,0 +1,39 @@
+"""
+    Hua Sun
+    2025-06-17 v0.33
+
+"""
+
+import argparse
+import pandas as pd
+import os
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', required=True, default='', help='input file')
+args = parser.parse_args()
+
+
+def main():
+    path = os.path.dirname(__file__)
+    config = ReadYaml(args.input)
+    r_script = config['rscript']
+
+    # 1
+    print('[INFO] Extraction data ...')
+    fscript = f'{path}/src/seu2mtx.R'
+    cmd = f'{r_script} {fscript} {args.input}'
+    os.system(cmd)
+
+    # 2
+    print('[INFO] Call CNV ...')
+    fscript = f'{path}/src/infercnv.R'
+    cmd = f'{r_script} {fscript} {args.input}'
+    os.system(cmd)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
