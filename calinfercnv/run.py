ab8e563dcb937481995cf892587cc4e49dfdfff2
Hua Sun <33794673+hsun9@users.noreply.github.com>
2025-06-20 12:35:56 -0400

    Add files via upload

--- calinfercnv/run.py
+++ calinfercnv/run.py
@@ -16,12 +16,13 @@ args = parser.parse_args()
 
 def main():
     path = os.path.dirname(__file__)
-    config = ReadYaml(args.input)
+    with open(args.input, 'r') as f:
+        config = yaml.safe_load(f)
     r_script = config['rscript']
 
     # 1
     print('[INFO] Extraction data ...')
-    fscript = f'{path}/src/seu2mtx.R'
+    fscript = f'{path}/src/seu2mtx.v2.R'
     cmd = f'{r_script} {fscript} {args.input}'
     os.system(cmd)
 
